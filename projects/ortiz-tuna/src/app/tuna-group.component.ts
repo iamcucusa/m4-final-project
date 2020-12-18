529eb0e619569d421b83da8a5148818d6157ebbe
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2020-12-18 14:27:17 +0100

    force video play on click

--- projects/ortiz-tuna/src/app/tuna-group.component.ts
+++ projects/ortiz-tuna/src/app/tuna-group.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'ot-tuna-group',
@@ -7,10 +7,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TunaGroupComponent implements OnInit {
 
+  // @ts-ignore
+  @ViewChild('fishVideo', {static: true}) tileFishVideoRef: ElementRef<HTMLVideoElement>;
+
   constructor() {
   }
 
-  ngOnInit(): void {
+  onMouseClick(): void {
+    this.tileFishVideoRef.nativeElement.play();
   }
 
+  ngOnInit(): void {
+    this.tileFishVideoRef.nativeElement.addEventListener('click', this.onMouseClick.bind(this));
+  }
 }
